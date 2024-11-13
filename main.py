0a41443c3b211e514087864144c7bf2d7753f029
Buezwqwg <buezwqwg@outlook.com>
2024-11-13 12:06:49 -0500

    udpate

--- main.py
+++ main.py
@@ -19,21 +19,24 @@ with sync_playwright() as p:
 
     page = context.new_page()
 
-    page.goto('https://recreation.utoronto.ca/booking/9720eff9-0499-4a71-b126-115879e67a16')
-    thrid_day = "xpath=/html/body/div[1]/div/div[3]/div[1]/div[2]/div[10]/div[2]/div[2]/button[3]"
-    page.locator(thrid_day).click()
-    lastest_time = 'xpath=/html/body/div[1]/div/div[3]/div[1]/div[2]/div[12]/div[2]/div[15]/div/button'
-
+    page.goto('https://recreation.utoronto.ca/booking/c394b167-188b-4846-994c-61e752697cc4')
+    second_day = "xpath=/html/body/div[1]/div/div[3]/div[1]/div[2]/div[10]/div[2]/div[2]/button[2]"
+    third_day = 'xpath=xpath=/html/body/div[1]/div/div[3]/div[1]/div[2]/div[10]/div[2]/div[2]/button[2]'
+    page.locator(second_day).click()
+    lastest_time = 'xpath=/html/body/div[1]/div/div[3]/div[1]/div[2]/div[12]/div[2]/div[2]/div/button'
+    court_two = 'xpath=/html/body/div[1]/div/div[3]/div[1]/div[2]/div[11]/div[2]/div/button[2]'
+    page.locator(court_two).click()
     def timer(t):
         while 1:
             current_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
             print(current_time)
             if current_time == t:
                 start_time = current_time
+                page.locator(third_day).click()
                 page.locator(lastest_time).click()
                 after_time = datetime.now().strftime("%H:%M:%S.%f")[:-3]
                 break
         print(start_time,after_time)
 
-    timer("21:48:35.660")
+    timer("11:59:59.900")
     time.sleep(1000)
